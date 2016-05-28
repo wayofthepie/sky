3975060cb3785fbe2400dab4b17882a27e902674
System administrator <wayofthepie@gmail.com>
2016-05-28 11:30:55 +0000

    Update compton settings to fix terminal issues
    Using xrender instead of gfx as the backend for compton fixes the issue
    with disappearing lines in xterm.

--- gfx/xmonad.hs
+++ gfx/xmonad.hs
@@ -1,23 +1,31 @@
 import XMonad
-
 import XMonad.Hooks.SetWMName
+import XMonad.Util.EZConfig
+import XMonad.Util.Paste
 
+import qualified Data.Map as M
 
-myStartupHook :: X ()
-myStartupHook = do
-  spawn "compton -f -I 0.10 -O 0.10 --backend glx --vsync opengl"
+main = xmonad $ defaultConfig
+  { borderWidth = 1
+  -- Focused border
+  , focusedBorderColor = "#ff0000"
+
+  -- Mod key binding
+  , modMask     = mod4Mask
+
+  -- Xmonad + swing integration
+  , startupHook = customStartupHook
+
+  -- Terminal configuration
+  , terminal    = "xterm -bg black -fg white -fa Monospace -fs 12"
+
+  }
+  `additionalKeys` copyPasteKeyBinding
+
+customStartupHook :: X ()
+customStartupHook = do
+  spawn "compton -f -I 0.10 -O 0.10 --backend xrender --vsync opengl"
   spawn "notify-send 'Xmonad Started/Recompiled' --icon=emblem-system"
   setWMName "LG3D" >> pure ()
 
-
-main = xmonad defaultConfig
-  { terminal    = "xterm -bg black -fg white -fa Monospace -fs 12"
-  , modMask     = mod4Mask
-  , borderWidth = 1
-
-  -- Xmonad + swing integration
-  , startupHook = myStartupHook
-
-  -- Focused border
-  , focusedBorderColor = "#ff0000"
-  }
+copyPasteKeyBinding = [ ((mod1Mask, xK_Insert), pasteSelection) ]
